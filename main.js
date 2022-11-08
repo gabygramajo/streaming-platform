1f412daa4523917b12345a099a3958304b7ba821
gaby gramajo <ggabrieldeveloper@gmail.com>
2022-11-07 23:33:25 -0300

    create scroll container

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const categoryNames = document.querySelectorAll(".category-name");
 const inputSearch = document.querySelector("#input-search");
 const userContainerInfo = document.querySelector(".user-container-info");
 const hamburgerMenuIcon = document.querySelector(".hamburger-menu-icon");
+const leftArrowIcons = document.querySelectorAll(".left-arrow-icon");
+const rightArrowIcons = document.querySelectorAll(".right-arrow-icon");
 
 
 inputSearch.classList.add('inactive');
@@ -25,3 +27,23 @@ function classListToggle( nodeList, className) {
     node => node.classList.toggle(className) 
     );
 }
+
+leftArrowIcons.forEach(leftArrowIcon => {
+  
+  leftArrowIcon.addEventListener('click', (e) => {
+    const viewComponentsContainer = e.composedPath()[2].childNodes[3];
+    viewComponentsContainer.scrollLeft -= 200;
+  });
+
+})
+
+rightArrowIcons.forEach( rightArrowIcon => {
+  
+  rightArrowIcon.addEventListener('click', (e) => {
+    const viewComponentsContainer = e.composedPath()[2].childNodes[3];
+    viewComponentsContainer.scrollLeft += 200;
+  });
+
+});
+
+
